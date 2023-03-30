d1a33933fb0d8964d3f24170922e3e33d99db893
martinezdelprenois <martintumwine@gmail.com>
2023-03-31 01:16:18 +0300

    added some more changes

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,28 +1,50 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import Container from 'react-bootstrap/esm/Container'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCapsules, faCashRegister, faCircleUser, faLayerGroup, faPerson } from '@fortawesome/free-solid-svg-icons';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 import './dashboard.scss';
 
 export default class DashboardPage extends Component {
   render() {
     return (
-<Container fluid className="p-0" >
+      <Container fluid className="p-0" >
 
-       
-
-        <div className='dashboard-content'> 
-        <div className='dashboard-header'></div>
-        <div className='side-menu'> 
-        <ul> 
-        <li> Summary </li>
-        <li> Stock </li>
-        <li> Customer </li>
-        <li> Sale </li>
-        </ul>
-        </div>
+        <div className='dashboard-content'>
+          <div className='side-menu'>
+            <div>  <FontAwesomeIcon icon={faLayerGroup} size="lg" className="awesome-icon" /> <span className='side-menu-item'> Summary </span> </div>
+            <div> <FontAwesomeIcon icon={faCapsules} size="lg" className="awesome-icon" /> <span className='side-menu-item'> Stock </span>  </div>
+            <div> <FontAwesomeIcon icon={faPerson} size="lg" className="awesome-icon" /> <span className='side-menu-item'> Customer </span>  </div>
+            <div> <FontAwesomeIcon icon={faCashRegister} size="lg" className="awesome-icon" /> <span className='side-menu-item'> Sales </span>   </div>
+          </div>
 
           <div className='dashboard-page'>
+
+            <div className='dashboard-header'>
+              <Navbar expand="lg">
+                <Container fluid>
+                <Nav.Item className='username'> Logo </Nav.Item>
+                  <Navbar.Toggle aria-controls="navbarScroll" />
+                  <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                      className="me-auto my-2 my-lg-0"
+                      style={{ maxHeight: '100px' }}
+                      navbarScroll>
+                      <Nav.Item> Organisation </Nav.Item>                     
+                    </Nav>
+                    <Nav.Item className='username'> Username </Nav.Item>
+                      <FontAwesomeIcon icon={faCircleUser} size="2xl" style={{color: "#144e2f",}} />
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
             </div>
+
+            
+          </div>
 
         </div>
       </Container>
